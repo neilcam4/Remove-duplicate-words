322c86f9c55cab910a4dd053f089119dc82beca8
Neil Campbell <neildcampbell@hotmail.com>
2019-10-14 06:42:05 +0100

    added 2 new tests check split on string

---
+++ spec/RemoveDuplicateSpec.js
@@ -0,0 +1,30 @@
+describe('remove duplicate strings', function(){
+    it('describes sort dtring into individual words', function(){
+        var string = 'alpha beta gamma gamma'
+        var result = removeDuplicateWords(string)
+        expect(result).toEqual(['alpha', 'beta', 'gamma', 'gamma'])
+    })
+    it('sorts array into alphabetical order', function(){
+        var string = 'alpha beta gamma gamma alpha'
+        var result = removeDuplicateWords(string)
+        expect(result).toEqual(['alpha', 'alpha', 'beta', 'gamma', 'gamma'])
+    })
+    it('should check if not duplicate and push to array', function(){
+        var string = 'alpha beta gamma gamma alpha'
+        var result = removeDuplicateWords(string)
+        expect(result).toEqual(['alpha'])
+    })
+})
+
+// Your task is to remove all duplicate words from a string, 
+// leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
---
+++ src/RemoveDuplicate.js
@@ -0,0 +1,10 @@
+function removeDuplicateWords(string){
+    var newArray = string.split(' ').sort()
+ 
+// if (string.length > 0) {
+//     newArray.forEach(function(item){
+//         newArray.push(item)
+//     })
+    return newArray
+// }
+}
